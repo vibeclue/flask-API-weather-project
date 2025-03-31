f1563c6cf77b025fc8383a0a13f84c58306ba39c
vbcl <most.possible@mail.ru>
2025-03-31 14:24:22 +0300

    add endpoints for yearly and stations

--- app.py
+++ app.py
@@ -22,5 +22,19 @@ def get_weather(station, date):
             "temperature": temperature}
 
 
+@app.route('/api/v1/<station>')
+def all_data_from_station(station):
+    filename = 'data-small/TG_STAID' + str(station).zfill(6) + '.txt'  # добавляю недостающие нули
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    return df.to_dict(orient="records")
+
+
+@app.route('/api/v1/yearly/<station>/<year>')
+def yearly(station, year):
+    filename = 'data-small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    return df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
